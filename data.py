65ce0e25fd5d092b311b6f9709aed27b97caf768
Terapat <terapatchattrakul@gmail.com>
2017-08-20 15:44:03 +0700

    2

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 data = {
-    'name': 'James Tiberius Kirk',
-    'nickname': 'Jim',
-    'image': 'kirk.jpg',
+    'name': 'Terapat Chattrakul',
+    'nickname': 'Toey',
+    'image': 'unnamed.png',
     'greeting_msg': 'Hi, there!',
     'about': """
         James Tiberius "Jim" Kirk was a male Human Starfleet officer in the 23rd century. He was arguably one of the most famous and highly decorated starship captains in the history of Starfleet. As the commanding officer of the Constitution-class starships USS Enterprise and USS Enterprise-A, Kirk served United Federation of Planets interests as an explorer, soldier, time-traveler, and diplomat.
